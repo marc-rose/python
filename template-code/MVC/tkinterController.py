6244870118386d1400ff2225114ff4561e10a66f
mrose <cram.esor@gmail.com>
2020-06-28 17:17:42 -0700

    Add tkinter MVC files

---
+++ template-code/MVC/tkinterController.py
@@ -0,0 +1,42 @@
+import tkinter
+import tkinterView  # the VIEW
+import tkinterModel  # the MODEL
+
+
+class templateController:
+    """
+    The CONTROLLER for an app that follows the MODEL/VIEW/CONTROLLER architecture.
+    When the user presses a Button on the VIEW, this CONTROLLER calls the appropriate
+    functions in the MODEL. The CONTROLLER handles all communication between the MODEL
+    and the VIEW.
+    """
+    def __init__(self):
+        """
+        This starts the Tk framework up, Instantiates the VIEW (a MyFrame object),
+        and starts the event loop that waits for the user to press a Button on the VIEW.
+        """
+        root = tkinter.Tk()
+        self.model = tkinterModel  # reference ConvertModel module (not class) for functions (not methods) later
+        self.view = tkinterView.templateView(self)  # self.view is an instance of the templateView Class
+        self.view.mainloop()
+        root.destroy()
+
+    def buttonAPressed(self):
+        """
+        When function_a_Button is pressed in the VIEW, the VIEW calls this function
+        """
+        entry = self.view.inputEntry.get()  # get text value from VIEW inputEntry widget
+        self.view.outputLabel["text"] = "function_a return: " + self.model.function_a(entry)
+        # set VIEW's Label text from MODEL's function output
+
+    def buttonBPressed(self):
+        """
+        When function_a_Button is pressed in the VIEW, the VIEW calls this function
+        """
+        entry = self.view.inputEntry.get()  # get text value from VIEW inputEntry widget
+        self.view.outputLabel["text"] = "function_b return: " + self.model.function_b(entry)
+        # set VIEW's Label text from MODEL's function output
+
+
+if __name__ == '__main__':
+    program = templateController()  # create a templateController object
---
+++ template-code/MVC/tkinterView.py
@@ -0,0 +1,46 @@
+import tkinter
+
+
+class templateView(tkinter.Frame):
+    """
+    class templateView is the VIEW for a simple program that exemplifies the MODEL/VIEW/CONTROLLER
+    architecture. This VIEW class is a tkinter.Frame that contains Buttons, an Entry, and a Label.
+    Two Buttons notify the CONTROLLER when they are pressed, and the other Button quits the app. The Label displays
+    the return value called function in the MODEL.
+    """
+
+    def __init__(self, controller):
+        """
+        Initializes the templateView object, creates a reference to the CONTROLLER, and creates widgets
+        """
+        tkinter.Frame.__init__(self)
+        self.pack()
+        self.controller = controller  # creates reference to controller object sent as input
+        self.createWidgets()
+
+    def createWidgets(self):
+        """
+        Creates all widgets for tkinter.Frame object
+        """
+        self.inputEntry = tkinter.Entry()  # tkinter.Entry for text input
+        self.inputEntry.insert(0, "Input Entry")  # default text
+        self.inputEntry.pack({"side": "left"})
+
+        self.function_a_Button = tkinter.Button(self)  # tkinter.Button to be pressed
+        self.function_a_Button["text"] = "call function_a"
+        self.function_a_Button["command"] = self.controller.buttonAPressed  # calls CONTROLLER function
+        self.function_a_Button.pack({"side": "left"})  # positioning of widget
+
+        self.function_b_Button = tkinter.Button(self)  # tkinter.Button to be pressed
+        self.function_b_Button["text"] = "call function_b"
+        self.function_b_Button["command"] = self.controller.buttonBPressed  # calls CONTROLLER function
+        self.function_b_Button.pack({"side": "left"})  # positioning of widget
+
+        self.outputLabel = tkinter.Label(self)  # tkinter.Label for displaying text output
+        self.outputLabel["text"] = 0  # called from CONTROLLER and text replaced
+        self.outputLabel.pack({"side": "left"})  # positioning of widget
+
+        self.quitButton = tkinter.Button(self)  # tkinter.Button to be pressed
+        self.quitButton["text"] = "Quit"
+        self.quitButton["command"] = self.quit  # calls tkinter.Button.quit function
+        self.quitButton.pack({"side": "left"})  # positioning of widget
